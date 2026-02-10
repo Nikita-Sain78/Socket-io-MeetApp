471f27a2c6c2fe0ef783537521d20df10eefc806
nikitasain@designingsolutions.co.in <nikitasain@designingsolutions.co.in>
2026-02-10 12:28:53 +0530

    changes done

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,11 +1,13 @@
-import mongoose from "mongoose"
-const MONGO_URI="mongodb://127.0.0.1:27017/meetapp"
+import mongoose from "mongoose";
+import { configDotenv } from "dotenv";
+configDotenv();
+const MONGO_URI = process.env.MONGO_URL;
 const dbConnection = async () => {
-    try {
-        await mongoose.connect(MONGO_URI); 
-        console.log("MongoDB connected successfully");
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-    }
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
 };
 export default dbConnection;
